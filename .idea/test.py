1f41fb35f899138e9ccfea36d08d4bfe3f2cb850
wangjinjing1984 <147446520@qq.com>
2022-05-19 16:46:25 +0800

    test used.

---
+++ .idea/test.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+# @FileName : test.py
+# @Author   : Jim
+# @Date     : 2022-05-19 16:30
+
+def main():
+    print("pycharm commit github.")
+
+
+if __name__ == '__main__':
+    main()
